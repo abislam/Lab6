6334b4c58b17232275bc8e1dc9abf67c0e10df59
Abed <aislam360@gmail.com>
2019-10-16 14:03:44 -0400

    Initial commit

---
+++ Lab6.java
@@ -0,0 +1,41 @@
+
+/*
+Implement a Connect 4 game. 
+
+The program should allow two players to play each other. 
+
+For each turn, have the user enter the column they want to play in, 
+validate the user has made a good move, 
+and then place the piece on the board. 
+
+Then the program should check for a winner and a tie, 
+and assuming there is neither, redisplay the board and get the next players move.
+
+Display the moves using print/println commands, 
+and use an X for player 1 and an O for player 2. 
+
+When the game ends, the program should display congratulations to the winner or 
+announce there was a tie, 
+and then allow the players to choose to play again or exit.
+
+You must use a 2D array for this lab.
+
+*/
+
+
+public class Lab6{
+	public static void main(String[] args){
+
+
+		//create 2d array for game board : 6 rows 7 columns
+		/*
+		get user input for column and row from user:
+		within user column, if row - 1 is not filled, 
+			make that the user filled column
+			use a for loop or while loop to check if rows below are filled
+
+
+		*/
+
+	}
+}
